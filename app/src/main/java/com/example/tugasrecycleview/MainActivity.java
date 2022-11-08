5bf5d84ee025d5b5cef9348f1ff0b18710903eab
deazuri <deazuriraihan@gmail.com>
2022-11-08 17:37:04 +0700

    Tugas Praktikum BAB 5

---
+++ app/src/main/java/com/example/tugasrecycleview/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.tugasrecycleview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
+    RecyclerView recylerView;
+
+    String s1[], s2[], s3[];
+    int images[] =
+            {R.drawable.perfectvelvet,R.drawable.tempo,R.drawable.universe,R.drawable.psycho};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_message){
+                    Intent a = new Intent(MainActivity.this,
+                            MainActivity.class);
+                    startActivity(a);
+                }else if (id == R.id.nav_chat){
+                    Intent a = new Intent(MainActivity.this,
+                            MainActivity2.class);
+                    startActivity(a);
+                }else if (id == R.id.nav_profile){
+                    Intent a = new Intent(MainActivity.this,
+                            MainActivity3.class);
+                    startActivity(a);                 }
+
+            return true;
+        }
+        });
+        recylerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.album);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        AlbumAdapter appAdapter = new AlbumAdapter(this, s1, s2, s3, images);
+        recylerView.setAdapter(appAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager((this), RecyclerView.HORIZONTAL, false);
+        recylerView.setLayoutManager(layoutManager);
+    }
+        @Override
+        public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    } }
